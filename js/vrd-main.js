debd6a633778b8aadb8d9c9664943d0b7a05819e
gui machiavelli <hey@guimachiavelli.com>
2013-03-24 00:56:25 +0100

    added: slideshow and started with the carousel

--- js/vrd-main.js
+++ js/vrd-main.js
@@ -1,21 +1,23 @@
 require.config({
   paths: {
+	//libraries
 	'modernizr': 'libs/modernizr',
     'jquery': [
 		'//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min',
-		'js/libs/jquery'
+		'libs/jquery.min'
 	],
     'underscore': [
 		'libs/underscore/underscore-min'
 	],
     'backbone': [
 		'libs/backbone/backbone-min'
-	]
+	],
+	//plugins
+	'carousel': 'libs/jcarousel/dist/jquery.jcarousel.min',
+	'carousel2': 'libs/filementgroup-responsive-carousel/dist/responsive-carousel'
+	
   }
 });
 
-requirejs(['modernizr', 'jquery', 'underscore', 'backbone'],
-function   (modernizr, $, unders_, backbone) {
-    //jQuery, canvas and the app/sub module are all
-    //loaded and can be used here now.
-});
+require(['libs/modernizr']);
+require(['modules/carousel', 'modules/slideshow']);
