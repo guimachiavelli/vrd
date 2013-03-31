8d6df23400bda74ac7d4e0ab19fa499d2c321c02
gui machiavelli <hey@guimachiavelli.com>
2013-03-31 01:20:30 +0100

    constant white space on nav scrolls

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -1,11 +1,11 @@
 define(['jquery'], function($) {
 
 	$('#menu a').click(function() {
-		var target = $(this.hash), menu_height = $('#menu').height() + 40;
+		var target = $(this.hash), menu_height = $('#menu').height();
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
-                 scrollTop: target.offset().top - menu_height
+                 scrollTop: target.offset().top - (menu_height + 20)
             }, 1500);
             return false;
         }
