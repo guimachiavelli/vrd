c94caf2598018d0f1ecd215607022747dd9d23b0
gui machiavelli <hey@guimachiavelli.com>
2013-03-31 01:34:08 +0100

    text, line and letter spacing adjustments

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -5,7 +5,7 @@ define(['jquery'], function($) {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
-                 scrollTop: target.offset().top - (menu_height + 20)
+                 scrollTop: target.offset().top - (menu_height + 22)
             }, 1500);
             return false;
         }
