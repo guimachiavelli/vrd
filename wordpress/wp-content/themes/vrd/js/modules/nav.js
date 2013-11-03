0a0631b902e9090292e25f21c73d23b8d1954b77
gui machiavelli <hey@guimachiavelli.com>
2013-11-03 20:35:05 +0000

    code formatting

--- wordpress/wp-content/themes/vrd/js/modules/nav.js
+++ wordpress/wp-content/themes/vrd/js/modules/nav.js
@@ -9,10 +9,10 @@ define(['jquery'], function($) {
 
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top - (menu_height + 22)
-            }, 1500);
-            return false;
+           $('html,body').animate({
+                scrollTop: target.offset().top - (menu_height + 22)
+           }, 1500);
+           return false;
         }
 
 	});
